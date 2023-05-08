d6ffbf1ec6ad906f1aa1433c387eb688ca91679c
rediska235 <vanya.bobr.12@gmail.com>
2023-05-08 15:13:34 +0300

    Add database

---
+++ Meetup.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Meetup.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Meetup.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Default");
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString, o => o.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName));
+        });
+
+        return services;
+    }
+}
